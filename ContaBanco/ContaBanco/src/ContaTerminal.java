7ff3e3723145a39cd67f45308a906e7faae51cd0
SinK3R <brunoskidrow@gmail.com>
2024-04-25 18:05:39 -0300

    Conta Terminal DIO

--- ContaBanco/ContaBanco/src/ContaTerminal.java
+++ ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o núemro da sua agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor do seu saldo");
+        double saldo = scanner.nextDouble(); 
+
+        System.out.println("Seja bem vindo " + nomeCliente + "!" + " O número da sua conta é: " + agencia + ", com um saldo de: " + saldo + " valor em reais");
     }
 }
